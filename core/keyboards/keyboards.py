950f2d2dc9a395409e228abee4754eee48938cf8
sk27110 <sashakoch27@gmail.com>
2024-03-03 10:19:24 +0300

    распределение функций по файлам

--- core/keyboards/keyboards.py
+++ core/keyboards/keyboards.py
@@ -7,6 +7,11 @@ from aiogram.types import(
     ReplyKeyboardRemove
 )
 
+from magic_filter import F
+from typing import Optional
+from aiogram.filters.callback_data import CallbackData
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
 registration_kb = ReplyKeyboardMarkup(
     keyboard=[
         [
@@ -120,5 +125,108 @@ from aiogram.types import (
 
 
 
+class NumbersCallbackFactory(CallbackData, prefix="fabnum"):
+    action: str
+    value: Optional[int] = None
+    name: str
 
+
+def get_keyboard_cp():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Типичная стенокардия", callback_data=NumbersCallbackFactory(action="set_cp", value=0, name = "Типичная стенокардия")
+    )
+    builder.button(
+        text="Атипичная стенокардия", callback_data=NumbersCallbackFactory(action="set_cp", value=1, name = "Атипичная стенокардия" )
+    )
+    builder.button(
+        text="Ангинальная боль", callback_data=NumbersCallbackFactory(action="set_cp", value=2, name = "Ангинальная боль")
+    )
+    builder.button(
+        text="Бессимптомно", callback_data=NumbersCallbackFactory(action="set_cp", value=3, name="Бессимптомно")
+    )
+    # Выравниваем кнопки по 4 в ряд, чтобы получилось 4 + 1
+    builder.adjust(2)
+    return builder.as_markup()
+
+
+
+def get_keyboard_fbs():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="<120", callback_data=NumbersCallbackFactory(action="set_fbs", value=0, name = "<120")
+    )
+    builder.button(
+        text=">=120", callback_data=NumbersCallbackFactory(action="set_fbs", value=1, name = ">=120" )
+    )
+    builder.adjust(2)
+    return builder.as_markup()
+
+
+
+def get_keyboard_restech():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Нормальное", callback_data=NumbersCallbackFactory(action="set_restecg", value=0, name = "Нормальное")
+    )
+    builder.button(
+        text="наличие аномалий ST-T (инверсия зубца Т и/или элевация или депрессия ST > 0,05 мВ)", callback_data=NumbersCallbackFactory(action="set_restecg", value=1, name = "аномалия ST-T" )
+    )
+    builder.button(
+        text="Наличие гипертрофии левого желудочка по критериям Эстеса ", callback_data=NumbersCallbackFactory(action="set_restecg", value=2, name = "гипертрофия желудочка")
+    )
+    builder.adjust(1)
+    return builder.as_markup()
+
+
+
+def get_keyboard_exng():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Есть", callback_data=NumbersCallbackFactory(action="set_exng", value=0, name = "Есть")
+    )
+    builder.button(
+        text="Нетю)", callback_data=NumbersCallbackFactory(action="set_exng", value=1, name = "Нетю" )
+    )
+
+    builder.adjust(1)
+    return builder.as_markup()
+
+
+def get_keyboard_slope():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Нисходящий", callback_data=NumbersCallbackFactory(action="set_slope", value=0, name = "Нисходящий")
+    )
+    builder.button(
+        text="Плоский", callback_data=NumbersCallbackFactory(action="set_slope", value=1, name = "Плоский" )
+    )
+    builder.button(
+        text="Восходящий", callback_data=NumbersCallbackFactory(action="set_slope", value=2, name = "Восходящий")
+    )
+    builder.adjust(1)
+    return builder.as_markup()
+
+
+
+
+def get_keyboard_ca():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="0", callback_data=NumbersCallbackFactory(action="set_ca", value=0, name = "0")
+    )
+    builder.button(
+        text="1", callback_data=NumbersCallbackFactory(action="set_ca", value=1, name = "1" )
+    )
+    builder.button(
+        text="2", callback_data=NumbersCallbackFactory(action="set_ca", value=2, name = "2")
+    )
+    builder.button(
+        text="3", callback_data=NumbersCallbackFactory(action="set_ca", value=3, name = "3" )
+    )
+    builder.button(
+        text="4", callback_data=NumbersCallbackFactory(action="set_ca", value=4, name = "4")
+    )
+    builder.adjust(3)
+    return builder.as_markup()
 
