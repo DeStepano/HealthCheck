32bc03533fd10b663ff58b904a64ed6184a7c03d
sk27110 <sashakoch27@gmail.com>
2024-03-30 21:48:54 +0300

    deleting photos from account

--- core/handlers/delete_acc.py
+++ core/handlers/delete_acc.py
@@ -5,7 +5,7 @@ import logging
 import sqlite3 as sl
 
 import asyncio
-
+import os
 from aiogram import Bot, Dispatcher, F, Router
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -24,6 +24,9 @@ async def change_user_data(message: Message, state: FSMContext):
     id_user = await get_hash(message.from_user.id)
     users = sl.connect('core/users.db')
     cursor = users.cursor()
+    brain_photo = cursor.execute("SELECT brain_photo FROM users WHERE id=?", [id_user]).fetchone()[0]
+    if brain_photo != None:
+        os.remove(f"{brain_photo}")
     cursor.execute("DELETE FROM users WHERE id = ?", (id_user,))
     cursor.execute("UPDATE users SET id = id - 1 WHERE id > ?", (id_user,))
     users.commit()
