3f38c808625b33b021f3976be5bb2bc6c0104cad
sk27110 <sashakoch27@gmail.com>
2024-05-25 02:50:27 +0300

    add_brain_analysis

--- core/brain_analysis.py
+++ core/brain_analysis.py
@@ -9,15 +9,55 @@ from config import config
 import torch
 import torchvision.transforms as transforms
 from torch.autograd import Variable
+from torchvision import models
+from torch import nn
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host=config.host))
 
 channel = connection.channel()
-
 channel.queue_declare(queue=config.brain_analysis_queue)
 
-model = torch.load('ml/best_model.model', map_location=torch.device('cpu'))
+
+class AdvancedMRI_Classifier(nn.Module):
+    def __init__(self, num_classes):
+        super(AdvancedMRI_Classifier, self).__init__()
+        
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.25)
+        )
+        
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.25)
+        )
+        
+        self.fc_block = nn.Sequential(
+            nn.Linear(64 * 56 * 56, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_block(x)
+        return x
+model = AdvancedMRI_Classifier(4)
+model.load_state_dict(torch.load('ml/model_28', map_location=torch.device('cpu')))
+model.eval()
+
 
 def brain_analysis(image):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -28,26 +68,27 @@ def brain_analysis(image):
 ])
     image = data_transforms(image)
     image = image.unsqueeze(0)
-    model.eval()
+    ans = ""
     if torch.cuda.is_available():
-            images=Variable(images.cuda())
+        images=Variable(images.cuda())
     with torch.no_grad():
         output = model(image)
-        print(output.data)
         _,prediction=torch.max(output.data,1)
-        print(prediction)
-    return 1
+        if prediction == torch.tensor([0]):
+            ans = "Обнаружена менингиома, советуем обратиться к специалисту"
+        elif prediction == torch.tensor([1]):
+            ans = "Обнаружена глинома, советуем обратиться к специалисту"
+        elif prediction == torch.tensor([2]):
+            ans = "Обнаружена аномалия в гипофизе, советуем обратиться к специалисту"
+        else:
+            ans = "Аномалий не обнаружено"
+    return ans
 
 def on_request(ch, method, props, body):
     body = body[2:-1]
     decoded_data = base64.b64decode(body)
     image = Image.open(io.BytesIO(decoded_data))
-    res = brain_analysis(image)
-    response = ""
-    if res>0.5:
-        response = json.dumps("Обнаружена аномалия")
-    else:
-        response = json.dumps("Анломалий не обнаружено")
+    response = json.dumps(brain_analysis(image))
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id = \
@@ -60,3 +101,8 @@ channel.basic_consume(queue=config.brain_analysis_queue, on_message_callback=on_
 
 print(" [x] Awaiting RPC requests")
 channel.start_consuming()
+
+
+#1 менингиома
+#2 глинома
+#3 гипофиз
